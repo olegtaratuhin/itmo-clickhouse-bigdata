f6ec0257e6569a3b8eaa75960ebe1b0f5eb6befe
Oleg Taratukhin <32278810+olegtaratuhin@users.noreply.github.com>
2020-06-12 18:43:39 +0300

    Add show mv2 script

---
+++ client/show_mv_2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "---------------------------------------------------------------------------------------------"
+
+echo "CH1:"
+/usr/bin/clickhouse-client --host=ch1 --query="SELECT user_id, avgMerge(avg_amount) from itmo.avg_out_amount_distributed group by user_id limit 10"
+
+echo "---------------------------------------------------------------------------------------------"
+
+echo "CH2:"
+/usr/bin/clickhouse-client --host=ch2 --query="SELECT user_id, avgMerge(avg_amount) from itmo.avg_out_amount_distributed group by user_id limit 10"
+
+echo "---------------------------------------------------------------------------------------------"
+
+echo "CH3:"
+/usr/bin/clickhouse-client --host=ch3 --query="SELECT user_id, avgMerge(avg_amount) from itmo.avg_out_amount_distributed group by user_id limit 10"
+
+echo "---------------------------------------------------------------------------------------------"
+
+echo "CH4:"
+/usr/bin/clickhouse-client --host=ch4 --query="SELECT user_id, avgMerge(avg_amount) from itmo.avg_out_amount_distributed group by user_id limit 10"
+
+echo "---------------------------------------------------------------------------------------------"
