46f5f9cb77b86c3229a7535fd3b8fb37122aef90
Oleg Taratukhin <32278810+olegtaratuhin@users.noreply.github.com>
2020-06-12 18:47:10 +0300

    Fix script

--- client/upload_all_mv.sh
+++ client/upload_all_mv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-upload_mv.sh create_mv_avg_in_amount.sql
-upload_mv.sh create_mv_avg_out_amount.sql
-upload_mv.sh create_mv_cnt_in_user.sql
-upload_mv.sh create_mv_cnt_out_user.sql
-upload_mv.sh create_mv_mv_trans_amount_sum.sql
-upload_mv.sh create_mv_mv_trans_cnt_sum.sql
+./upload_mv.sh create_mv_avg_in_amount.sql
+./upload_mv.sh create_mv_avg_out_amount.sql
+./upload_mv.sh create_mv_cnt_in_user.sql
+./upload_mv.sh create_mv_cnt_out_user.sql
+./upload_mv.sh create_mv_trans_amount_sum.sql
+./upload_mv.sh create_mv_trans_cnt_sum.sql
