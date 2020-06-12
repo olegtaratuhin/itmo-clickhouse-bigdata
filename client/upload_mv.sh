c991562fa99be458ab1dfaddfedc5e7755fcb85a
Oleg Taratukhin <32278810+olegtaratuhin@users.noreply.github.com>
2020-06-12 18:37:09 +0300

    Update mv uploading

--- client/upload_mv.sh
+++ client/upload_mv.sh
@@ -6,6 +6,9 @@ if [ "$1" != "" ]; then
     cat $1
     echo 
     cat $1 | /usr/bin/clickhouse-client --host=ch1 -mn
+    cat $1 | /usr/bin/clickhouse-client --host=ch2 -mn
+    cat $1 | /usr/bin/clickhouse-client --host=ch3 -mn
+    cat $1 | /usr/bin/clickhouse-client --host=ch4 -mn
     echo 
 
     echo "Done"
