177e0ef9c7704db048afe3ded70cd96799af7bfb
Oleg Taratukhin <32278810+olegtaratuhin@users.noreply.github.com>
2020-06-12 19:10:00 +0300

    Add uploading data

--- client/clean_upload.sh
+++ client/clean_upload.sh
@@ -8,6 +8,10 @@ echo "Creating new database"
 ./create.sh
 echo "Database created"
 
+echo "Upload data"
+./upload_data.sh
+echo "Finished uploading data"
+
 echo "Creating MVs"
 ./upload_all_mv.sh
 echo "Created all MVs"
